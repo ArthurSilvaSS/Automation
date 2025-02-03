abe66a01ed4f21ce8a02399831da60481b51f4ba
Arthur Silva <arthur.silva443@gmail.com>
2025-02-03 19:10:38 -0300

    usando o readline como terminal para obter os dados para gerar o link de acesso

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
-const pup = require('puppeteer');
+const { resolve } = require('path');
+const puppeteer = require('puppeteer');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const perguntar = (pergunta) => {
+    return new Promise(resolve => {
+        rl.question(pergunta, resposta => {
+            resolve(resposta)
+        });
+    });
+};
 
 (async () => {
-    //iniciando o navegador e uma nova guia
-    const browser = await pup.launch();
-    const page = await browser.newPage();
+    const LocalBuscado = await perguntar('digite o comercio a ser buscado: ');
+    const CidadeBuscada = await perguntar('qual a cidade buscada: ');
 
-    console.log('Navegador iniciado !!');
-
-    //gera o link de busca
-    const LocalBuscado = prompt('digite o comercio a ser buscado');
-    const CidadeBuscada = prompt('qual a cidade buscada');
+    rl.close();
 
     const link = `https://www.google.com/search?q=${encodeURIComponent(LocalBuscado)}+${encodeURIComponent(CidadeBuscada)}`;
-
     console.log(link)
 
-    await page.goto(link);
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto(link, { waitUntil: 'load' });
 
     console.log('Acessa a pagina');
 
