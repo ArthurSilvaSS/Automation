f310fa87ddc28e58140aa120784ac34f2f45299a
Arthur Silva <arthur.silva443@gmail.com>
2025-02-03 18:57:59 -0300

    teste para geracao do link de busca

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const pup = require('puppeteer');
 
 (async () => {
+    //iniciando o navegador e uma nova guia
     const browser = await pup.launch();
     const page = await browser.newPage();
-    console.log('Inicia o navegador')
 
-    await page.goto('https://google.com')
-    console.log('Acessa a pagina')
+    console.log('Navegador iniciado !!');
+
+    //gera o link de busca
+    const LocalBuscado = prompt('digite o comercio a ser buscado');
+    const CidadeBuscada = prompt('qual a cidade buscada');
+
+    const link = `https://www.google.com/search?q=${encodeURIComponent(LocalBuscado)}+${encodeURIComponent(CidadeBuscada)}`;
+
+    console.log(link)
+
+    await page.goto(link);
+
+    console.log('Acessa a pagina');
 
     await browser.close();
-    console.log('finaliza o navegador')
+    console.log('finaliza o navegador');
 })();
